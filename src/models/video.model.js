1cc01d0a5b3be1dc82122d212cfd3925b5844a51
sourav <souravbag2090sb@gmail.com>
2025-07-21 00:53:48 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,34 @@
+
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema (
+    {
+        videoFile:{
+            type:String, // cloudinary url
+            required:true
+        },
+        title:{
+            type:String, // cloudinary url
+            required:true
+        },
+        duration:{
+            type:Number, // cloudinary url
+            required:true
+        },
+        views: {
+            type:Number,
+            default:0
+        },
+        isPublished: {
+            type:Boolean,
+            default:true
+        },
+        owner: {
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+    }
+)
+
+videoSchema.
+export const Video = mongoose.Schema("Video",videoSchema);
